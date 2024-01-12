e9af58cb9c5d27ee86f6208c17755eea0a821b88
haith4m <haith4mmohammed@gmail.com>
2024-01-12 12:06:02 +0000

    chelsea winning the carabao cup

---
+++ core/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from core.views import *
+
+app_name = "core"
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("login/", login, name="login"),
+    path("services/", services, name="services"),
+    path("register/", register, name="register"),
+    path("forgot-username/", forgot, name="forgot-username"),
+    path("space/", space, name="space"),
+    path("baking/", baking, name="baking"),
+    path("collection/", collection, name="collection"),
+    path("custom/", custom, name="custom"),
+    path("social-media/", social_media, name="social-media"),
+    path("confirmation", confirmation, name="confirmation"),
+    path("baking/", baking, name="baking"),
+
+
+
+]
--- core/views.py
+++ core/views.py
@@ -6,4 +6,39 @@ from django.http import HttpResponse
 def index(request):
     return render(request,'core/index.html')
 
+def login(request):
+    return render(request,'core/login.html')
+
+def services(request):
+    return render(request,'core/services.html')
+
+def register(request):
+    return render(request,'core/forgot.html')
+
+def forgot(request):
+    return render(request,'core/forgot-username.html')
+
+def space(request):
+    return render(request,'core/space.html')
+
+def baking(request):
+    return render(request,'core/baking.html')
+
+def collection(request):
+    return render(request,'core/collection.html')
+
+def custom(request):
+    return render(request,'core/custom.html')
+
+def social_media(request):
+    return render(request,'core/social-media.html')
+
+def confirmation(request):
+    return render(request,'core/confirmation.html')
+
+
+
+
+
+
 
